d86f25b8d12a8581de347ee1001a6b7e76f1783e
Alexander Cherkasov <alkorotitskij@gmail.com>
2025-06-13 13:31:02 +0300

    Сверстать hero

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,3 +1,5 @@
 import { BurgerButton } from './burger-button'
+import { SocialsModal } from './socials-modal'
 
 BurgerButton()
+SocialsModal()
---
+++ src/ts/socials-modal.ts
@@ -0,0 +1,25 @@
+export function SocialsModal() {
+	const button: HTMLElement | null = document.getElementById('hero-button')
+	const modal: HTMLElement | null = document.getElementById('socials-modal')
+	const body: HTMLElement | null = document.querySelector('body')
+
+	button?.addEventListener('click', (e) => {
+		e.stopPropagation()
+		modal?.classList.toggle('socials-modal--open')
+	})
+
+	body?.addEventListener('click', (e) => {
+		if (!modal?.classList.contains('socials-modal--open')) return
+
+		const target = e.target as Node
+		console.log(target)
+
+		if (!modal.contains(target) && target !== button) {
+			modal.classList.remove('socials-modal--open')
+		}
+	})
+
+	modal?.addEventListener('click', (event) => {
+		event.stopPropagation()
+	})
+}
